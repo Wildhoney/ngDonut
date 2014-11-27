0dcfd6f6c1f14fb87fc10fb6d2880aad5e29a8f0
Wildhoney <adam.timberlake@gmail.com>
2014-11-27 21:30:42 +0000

    Added basic file structure

--- components/ngDonut.js
+++ components/ngDonut.js
@@ -1,5 +1,51 @@
-(function main($angular) {
+(function main($angular, $d3) {
 
 	"use strict";
 
-})(window.angular);
+	/**
+	 * @method throwException
+	 * @param message {String}
+	 * @return {void}
+	 */
+	var throwException = function throwException(message) {
+		throw "ngDonut: " + message + ".";
+	};
+
+	if (typeof $angular === 'undefined') {
+		throwException('Angular is a required library: https://angularjs.org/');
+	}
+
+	if (typeof $d3 === 'undefined') {
+		throwException('D3 is a required library: http://d3js.org/');
+	}
+
+	/**
+	 * @module ngDonut
+	 * @author Adam Timberlake
+	 * @link https://github.com/Wildhoney/ngDonut
+	 */
+	$angular.module('ngDonut', []).directive('ngDonut', function ngDonut() {
+
+		return {
+
+			/**
+			 * @property restrict
+			 * @type {String}
+			 */
+			restrict: 'EA',
+
+			/**
+			 * @method link
+			 * @param scope {Object}
+			 * @param element {angular.element}
+			 * @return {void}
+			 */
+			link: function link(scope, element) {
+
+			}
+
+		};
+
+	});
+
+})(window.angular, window.d3);
