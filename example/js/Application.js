529a39a796cf992b7d1fe0d86cfffa707bb767a0
Wildhoney <adam.timberlake@gmail.com>
2014-11-27 21:43:07 +0000

    Configured example

---
+++ example/js/Application.js
@@ -0,0 +1,14 @@
+(function main($angular) {
+
+    // Бог тро́ицу лю́бит...
+    $angular.module('donutApp', ['ngDonut']).controller('DonutController', function DonutController($scope) {
+
+        /**
+         * @property donutModel
+         * @type {Object}
+         */
+        $scope.donutModel = {};
+
+    });
+
+})(window.angular);
